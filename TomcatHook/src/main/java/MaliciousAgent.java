21e758b5c3cba183324fca41874d79eb779578e1
liaozixin <1571155318@qq.com>
2024-12-09 23:28:42 +0800

    完成hook tomcat 启动项例子

---
+++ TomcatHook/src/main/java/MaliciousAgent.java
@@ -0,0 +1,25 @@
+import java.lang.instrument.Instrumentation;
+
+public class MaliciousAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("TomcatAgent is running...");
+        executeCommand();
+    }
+
+    private static void executeCommand() {
+        try {
+            // 执行 cat /etc/passwd 指令
+            Process process = Runtime.getRuntime().exec("whoami");
+            process.waitFor();
+
+            // 读取命令输出
+            java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
